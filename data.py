d6cc804baf07acfbd8d326dcee3b066161a51980
percicotte <nmpercicotte@gmail.com>
2026-06-22 15:59:21 -0300

    Initial commit

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-f2f24ed4-c066-48d0-b221-7ce6677d8a95.containerhub.tripleten-services.com?lng=pt'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
+
